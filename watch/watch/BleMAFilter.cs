9ec5a5b4a9d76b53e709bc16b1489041412962e8
Ignas <ignasandholly@gmail.com>
2022-01-01 22:40:55 +0000

    move ble in background

---
+++ watch/watch/BleMAFilter.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using System.Numerics;
+using Android.Util;
+
+namespace watch
+{
+    public class BleMaFilter
+    {
+        private const String TAG = "mono-stdout";
+        private float[] _accArr;
+        private float[] _gyroArr;
+        private const int NumberOfSamples = 3;
+
+        public BleMaFilter()
+        {
+            ClearAcc();
+            ClearGyro();
+        }
+        
+        public void AddAcc( Vector3 aVector3)
+        {
+            _accArr[0] += aVector3.X;
+            _accArr[1] += aVector3.Y;
+            _accArr[2] += aVector3.Z;
+        }
+
+        public void AddGyro(Vector3 gVector3)
+        {
+            _gyroArr[0] += gVector3.X;
+            _gyroArr[1] += gVector3.Y;
+            _gyroArr[2] += gVector3.Z;
+        }
+
+        public String GetAcc()
+        {
+            try
+            {
+                var totalNumber = _accArr.Select(x => x / NumberOfSamples);
+                return "A" + string.Join(",", totalNumber);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Log.Verbose(TAG, $"bleMAFilter error GETACC(){e}");
+                throw;
+            }
+            
+
+        }
+
+        public String GetGyro()
+        {
+            try
+            {
+                var totalNumber = _gyroArr.Select(x => x / NumberOfSamples);
+                return "G" + string.Join(",", totalNumber);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                Log.Verbose(TAG, $"bleMAFilter error GETGyro(){e}");
+
+                throw;
+            }
+            
+        }
+
+        public void ClearGyro()
+        {
+            _gyroArr = new float[]{0,0,0};
+        }
+        public void ClearAcc()
+        {
+            _accArr = new float[]{0,0,0};
+        }
+        
+    }
+}
---
+++ watch/watch/SensorService.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Util;
+
+namespace watch
+{
+    [Service]
+    public class SensorService : Service
+    {
+        const int SERVICE_RUNNING_NOTIFICATION_ID = 10000;
+        private const string TAG = "mono-stdout";
+        private BleServer bleServer;
+        private SensorManager sensorManager;
+
+        public override StartCommandResult OnStartCommand(Intent intent, StartCommandFlags flags, int startId)
+        {
+            StartForegroundNotification();
+             bleServer =  new BleServer(this);
+             sensorManager = new SensorManager();
+             SubscribeToSensor();
+            
+            return StartCommandResult.Sticky;
+        }
+
+        /**
+         * Subscribe to sensor and Server
+         * to communicate in between
+         */
+        private void SubscribeToSensor()
+        {
+            // sensor data transfer to BLE server
+            sensorManager.AccEventHandler += (s, e) =>
+            {
+                bleServer.SensorData.Enqueue(e.Data);
+                Log.Verbose(TAG,$"enqued:{e.Data}");
+
+
+            };
+            sensorManager.GyroEventHandler += (s, e) =>
+            {
+                bleServer.SensorData.Enqueue(e.Data);
+                
+            };
+
+            //Server communications
+            if (bleServer.BltCallback != null)
+            {
+                bleServer.BltCallback.DisconectHandler += (s, e) =>
+                {
+                    sensorManager.ToggleSensors();
+                };
+                
+                
+                bleServer.BltCallback.DataWriteHandler += (s, e) =>
+                {
+                    sensorManager.ToggleSensors();
+                    sensorManager.UnsubscribeSensors();
+                    StopForeground(true);
+                    StopSelf();
+                
+                };
+            }
+            
+        }
+
+
+        // -------------------------------------- Notification methods ---------------------------------------------------------
+        private void StartForegroundNotification()
+        {
+            CreateNotificationChannel();
+            try
+            {
+                var notification = new Notification.Builder(this,"999")
+                    .SetContentTitle("a")
+                    ?.SetContentText("b")
+                    ?.SetOngoing(true)
+                    ?.Build();
+                StartForeground(SERVICE_RUNNING_NOTIFICATION_ID, notification);
+            }
+            catch (Exception e)
+            {
+                Log.Verbose(TAG,e.Message);
+                throw;
+            }
+            
+        }
+
+        
+        private void CreateNotificationChannel() {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O) {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel("999", "channelName", NotificationImportance.Default) {
+                Description = "channelDescription"
+            };
+
+            var notificationManager = (NotificationManager) GetSystemService(NotificationService);
+            if (notificationManager != null) notificationManager.CreateNotificationChannel(channel);
+        }
+
+
+        public override IBinder OnBind(Intent intent)
+        {
+            return null;
+        }
+    }
+    
+}
