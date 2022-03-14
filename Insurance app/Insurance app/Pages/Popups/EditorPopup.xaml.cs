0e291eb61b2bc39c0ac93bba8e1f0eeb8c08d5d6
Ignas <ignasandholly@gmail.com>
2022-03-14 14:49:01 +0000

    .

---
+++ Insurance app/Insurance app/Pages/Popups/EditorPopup.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Insurance_app.SupportClasses;
+using Insurance_app.ViewModels.Popups;
+using Xamarin.CommunityToolkit.UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Insurance_app.Pages.Popups
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditorPopup: Popup<string>
+    {
+        public EditorPopup()
+        {
+            InitializeComponent();
+            BindingContext = new EditorViewModel(this);
+        }
+
+        private async void  Button_OnClicked(object sender, EventArgs e)
+        {
+   
+                var vm = (EditorViewModel)BindingContext;
+                
+                if (ExtraInfoValidator.IsValid)
+                {
+                    vm.CloseCommand.Execute(null);
+                }
+                else
+                {
+                    var errBuilder = new StringBuilder();
+                    if (ExtraInfoValidator.IsNotValid)
+                    {
+                        if (ExtraInfoValidator.Errors != null)
+                            foreach (var err in ExtraInfoValidator.Errors.OfType<string>())
+                            {
+                                errBuilder.AppendLine(err);
+                            }
+                    }
+                    await Application.Current.MainPage.DisplayAlert(Msg.Error, errBuilder.ToString(), "close");
+                } 
+        }
+    }
+}
---
+++ Insurance app/Insurance app/ViewModels/Popups/EditorViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows.Input;
+using Insurance_app.Pages.Popups;
+using Xamarin.CommunityToolkit.ObjectModel;
+using Xamarin.Forms;
+
+namespace Insurance_app.ViewModels.Popups
+{
+    
+    public class EditorViewModel : ObservableObject
+    {
+        private readonly EditorPopup popup;
+        public ICommand CloseCommand { get; }
+        public ICommand SubmitCommand { get; }
+        public EditorViewModel(EditorPopup popup)
+        {
+            this.popup = popup;
+            CloseCommand = new Command(Close);
+            SubmitCommand = new Command(Submit);
+        }
+
+        private void Submit()
+        {
+            popup.Dismiss(extraInfo);
+
+        }
+        private void Close()
+        {
+            popup.Dismiss("");
+        }
+
+        private string extraInfo;
+        public string ExtraInfoDisplay
+        {
+            get => extraInfo;
+            set => SetProperty(ref extraInfo, value);
+        }
+        
+        
+        
+        
+    }
+}
